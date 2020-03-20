d636900b1d05f27a8e31a7f19518e28e6208369d
Kevin Crum <kevinbaileycrum@gmail.com>
2020-03-19 21:41:22 -0700

    edited trivial cases for test framework

--- tests.py
+++ tests.py
@@ -5,10 +5,15 @@ BASE_URL = 'http://localhost:8000'
 
 class TestZipHrEndpoints(unittest.TestCase):
 
+    # loads index
     def test_trivial(self):
         req = requests.get(BASE_URL)
-        print(req.status_code)
-        # self.assertEqual(req.status_code, 200)
+        self.assertEqual(req.status_code, 200)
+
+    # trivial test for capacity endpoint
+    def test_capacity_no_args(self):
+        req = requests.get(BASE_URL)
+        self.assertEqual(req.status_code, 200)
 
 if __name__ == "__main__":
     unittest.main()
