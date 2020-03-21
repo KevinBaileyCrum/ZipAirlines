7983fe6c59c0864389d6563d4fb086104594a878
Kevin Crum <kevinbaileycrum@gmail.com>
2020-03-20 18:53:18 -0700

    more tests

--- ZipAirlines/api/views.py
+++ ZipAirlines/api/views.py
@@ -30,16 +30,18 @@ class CapacityView(APIView):
         logger.error(request.query_params)
         if request.query_params:
             try:
+                responseList = list()
                 assert (len(request.query_params.getlist('planeId')) == len(request.query_params.getlist('passengerNum'))), 'MISSING PARAMETER ERROR: include a planeId and passengerNum for each plane'
                 assert (len(request.query_params.getlist('planeId')) > 0 and len(request.query_params.getlist('planeId')) < 11), 'planeId ERROR: please ensure queried planeId(s) is/are between 1 and 10'
                 for planeId, passengerNum in zip(request.query_params.getlist('planeId'), request.query_params.getlist('passengerNum')):
                         assert (planeId.isnumeric() and passengerNum.isnumeric()), 'PARAMETER ERROR: please ensure both planeId and passengerNum are type int'
                         assert (float(planeId) > 0 and float(planeId) < 11), 'planeId ERROR: planeIds must be between 1 and 10'
                         minutesOfFlight = self.findCapacity(float(planeId), float(passengerNum))
-                        return Response(
-                            minutesOfFlight,
-                            status=200
-                        )
+                        responseList.append(('planeId '+planeId, minutesOfFlight))
+                return Response(
+                    responseList,
+                    status=200
+                )
             except AssertionError as error:
                 errorMsg = "ERROR-- "+str(error)
                 return Response(
--- tests.py
+++ tests.py
@@ -6,6 +6,10 @@ ENDPOINT = 'capacity/'
 
 class TestZipHrEndpoints(unittest.TestCase):
 
+    ###################################################################
+    # trivial test cases
+    ###################################################################
+
     # loads index
     def test_trivial(self):
         req = requests.get(BASE_URL)
@@ -16,6 +20,10 @@ class TestZipHrEndpoints(unittest.TestCase):
         req = requests.get(BASE_URL)
         self.assertEqual(req.status_code, 400)
 
+    ###################################################################
+    # poorly formed URL paramters
+    ###################################################################
+
     # test for capacity endpoint with letter in passengerNum param
     def test_for_capacity_endpoint_with_letter_in_passengerNum_param(self):
         request = BASE_URL + ENDPOINT + '?planeId=5&passengerNum=13z'
@@ -24,6 +32,14 @@ class TestZipHrEndpoints(unittest.TestCase):
 
     def test_for_capacity_endpoint_with_letter_in_planeId_param(self):
         request = BASE_URL + ENDPOINT + '?planeId=z5&passengerNum=13'
+        req = requests.get(request)
+        self.assertEqual(req.status_code, 401)
+
+    def test_for_capacity_endpoint_with_letter_in_middle_of_good_queries(self):
+        request = BASE_URL + ENDPOINT + '?planeId=3&passengerNum=1&planeId=1&passengerNum=5&planeId=4&passengerNum=l9'
+        req = requests.get(request)
+        self.assertEqual(req.status_code, 401)
+        request = BASE_URL + ENDPOINT + '?planeId=3&passengerNum=1&planeId=x1&passengerNum=5&planeId=4&passengerNum=9'
         req = requests.get(request)
         self.assertEqual(req.status_code, 401)
 
@@ -37,6 +53,29 @@ class TestZipHrEndpoints(unittest.TestCase):
         req = requests.get(request)
         self.assertEqual(req.status_code, 401)
 
+    def test_for_capacity_endpoint_with_missing_param_in_middle_of_good_queries(self):
+        request = BASE_URL + ENDPOINT + '?planeId=3&passengerNum=1&planeId=1&planeId=4&passengerNum=9'
+        req = requests.get(request)
+        self.assertEqual(req.status_code, 401)
+        request = BASE_URL + ENDPOINT + '?planeId=3&passengerNum=1&&planeId=4'
+        req = requests.get(request)
+        self.assertEqual(req.status_code, 401)
+
+    ###################################################################
+    # successful queries
+    ###################################################################
+    # test for capacity with one arg correctly formatted of the form:
+    # BASE_URL/capacity?id=[1-10]&num_passenger=1
+    def test_single_plane_properly_formatted(self):
+        request = BASE_URL + ENDPOINT +'?planeId=2&passengerNum=40'
+        req = requests.get(request)
+        self.assertEqual(req.status_code, 200)
+
+if __name__ == "__main__":
+    unittest.main()
+    ###################################################################
+    # successful queries
+    ###################################################################
     # test for capacity with one arg correctly formatted of the form:
     # BASE_URL/capacity?id=[1-10]&num_passenger=1
     def test_single_plane_properly_formatted(self):
