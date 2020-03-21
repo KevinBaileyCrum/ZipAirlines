8d1c1e0fbc1a5713fe121cc191b849f0495d0cda
Kevin Crum <kevinbaileycrum@gmail.com>
2020-03-20 18:03:23 -0700

    better handled assertion error messages by passing a string as second parameter on assertion

--- ZipAirlines/api/views.py
+++ ZipAirlines/api/views.py
@@ -8,7 +8,6 @@ logger = logging.getLogger(__name__)
 
 class IndexView(APIView):
     def get(self, request):
-        logger.error('hello i am a logger')
         return Response(
             'please send query to endpoint URL:port/capacity followed' \
             'by ordered planeId and passanger numebrs respectively',
@@ -24,30 +23,28 @@ class CapacityView(APIView):
         print(f'planeId: {planeId} passengerNum: {passengerNum}')
         fuelCapacity = 200 * planeId
         litersPerMinute = (log(planeId) * .80) + (.002 * passengerNum) # log base 2
-        minutesOfFlight = fuelCapacity / litersPerMinute
-        print(minutesOfFlight)
-        return minutesOfFlight
+        return fuelCapacity / litersPerMinute
 
     def get(self, request):
         logger.error('request')
         logger.error(request.query_params)
         if request.query_params:
-            for planeId, passengerNum in zip(request.query_params.getlist('planeId'), request.query_params.getlist('passengerNum')):
-                try:
-                    assert (planeId.isnumeric and passengerNum.isnumeric)
-                    minutesOfFlight = self.findCapacity(float(planeId), float(passengerNum))
-                    return Response(
-                        minutesOfFlight,
-                        status=200
-                    )
-                except AssertionError as error:
-                    errorMsg = 'PARAMETER ERROR: please ensure both planeId and passengerNum are type int {}'.format(error)
-                    return Response(
-                        errorMsg,
-                        status=401
-                    )
+            try:
+                assert (len(request.query_params.getlist('planeId')) == len(request.query_params.getlist('passengerNum'))), 'MISSING PARAMETER ERROR: include a planeId and passengerNum for each plane'
+                for planeId, passengerNum in zip(request.query_params.getlist('planeId'), request.query_params.getlist('passengerNum')):
+                        assert (planeId.isnumeric() and passengerNum.isnumeric()), 'PARAMETER ERROR: please ensure both planeId and passengerNum are type int'
+                        minutesOfFlight = self.findCapacity(float(planeId), float(passengerNum))
+                        return Response(
+                            minutesOfFlight,
+                            status=200
+                        )
+            except AssertionError as error:
+                errorMsg = "ERROR-- "+str(error)
+                return Response(
+                    errorMsg,
+                    status=401
+                )
 
-            return Response({'test': 'it worked'})
         else:
             return Response(
                 'please enter up to ten ordered int:->planeId, int:->num_passanger respectively'
--- tests.py
+++ tests.py
@@ -1,7 +1,8 @@
 import unittest
 import requests
 
-BASE_URL = 'http://localhost:8000'
+BASE_URL = 'http://localhost:8000/'
+ENDPOINT = 'capacity/'
 
 class TestZipHrEndpoints(unittest.TestCase):
 
@@ -15,8 +16,27 @@ class TestZipHrEndpoints(unittest.TestCase):
         req = requests.get(BASE_URL)
         self.assertEqual(req.status_code, 400)
 
+    # test for capacity endpoint with letter in passengerNum param
+    def test_for_capacity_endpoint_with_letter_in_passengerNum_param(self):
+        request = BASE_URL + ENDPOINT + '?planeId=5&passengerNum=13z'
+        req = requests.get(request)
+        self.assertEqual(req.status_code, 401)
+
+    def test_for_capacity_endpoint_with_letter_in_planeId_param(self):
+        request = BASE_URL + ENDPOINT + '?planeId=z5&passengerNum=13'
+        req = requests.get(request)
+        self.assertEqual(req.status_code, 401)
+
+    def test_for_missing_parameter_no_passengerNum(self):
+        request = BASE_URL + ENDPOINT + '?planeId=5'
+        req = requests.get(request)
+
     # test for capacity with one arg correctly formatted of the form:
     # BASE_URL/capacity?id=[1-10]&num_passenger=1
+    def test_single_plane_properly_formatted(self):
+        request = BASE_URL + ENDPOINT +'?planeId=2&passengerNum=40'
+        req = requests.get(request)
+        self.assertEqual(req.status_code, 200)
 
 if __name__ == "__main__":
     unittest.main()
