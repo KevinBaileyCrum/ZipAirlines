63661ec9e59c2ce28713897226bcbb2e280cfc52
Kevin Crum <kevinbaileycrum@gmail.com>
2020-03-20 17:28:43 -0700

    using base2 log, works for single argument

--- ZipAirlines/api/views.py
+++ ZipAirlines/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 import logging
+from math import log
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -20,19 +21,25 @@ class IndexView(APIView):
 class CapacityView(APIView):
 
     def findCapacity(self, planeId: int, passengerNum: int) -> int:
-        print(planeId)
-        print(passengerNum)
-        return 1
+        print(f'planeId: {planeId} passengerNum: {passengerNum}')
+        fuelCapacity = 200 * planeId
+        litersPerMinute = (log(planeId) * .80) + (.002 * passengerNum) # log base 2
+        minutesOfFlight = fuelCapacity / litersPerMinute
+        print(minutesOfFlight)
+        return minutesOfFlight
 
     def get(self, request):
         logger.error('request')
         logger.error(request.query_params)
         if request.query_params:
-            logger.error('i exist')
             for planeId, passengerNum in zip(request.query_params.getlist('planeId'), request.query_params.getlist('passengerNum')):
                 try:
-                    assert (isinstance(planeId and passengerNum, int))
-                    self.findCapacity(planeId, passengerNum)
+                    assert (planeId.isnumeric and passengerNum.isnumeric)
+                    minutesOfFlight = self.findCapacity(float(planeId), float(passengerNum))
+                    return Response(
+                        minutesOfFlight,
+                        status=200
+                    )
                 except AssertionError as error:
                     errorMsg = 'PARAMETER ERROR: please ensure both planeId and passengerNum are type int {}'.format(error)
                     return Response(
