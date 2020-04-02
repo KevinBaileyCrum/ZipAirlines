a7e2d8fa99b7922b99e99cf0991a85ea3ebe429a
Kevin Crum <kevinbaileycrum@gmail.com>
2020-04-01 17:32:28 -0700

    django root url imports class based view and class url handles

--- ZipAirlines/api/urls.py
+++ ZipAirlines/api/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from capacity.views import CapacityView
 
 urlpatterns = [
-    path('capacity/', include('capacity.urls')),
+    path('capacity/', CapacityView.as_view()),
     path('admin/', admin.site.urls),
 ]
 
--- ZipAirlines/capacity/views.py
+++ ZipAirlines/capacity/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the polls index.")
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class CapacityView(APIView):
+    def get(self, request):
+        data = request.data
+        return Response(data)
