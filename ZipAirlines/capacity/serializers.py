1d42e64bf7efff3aeb02484fff1b5b2fdb89208b
Kevin Crum <kevinbaileycrum@gmail.com>
2020-04-04 15:13:54 -0700

    added get endpoint for zero parameters passed

--- ZipAirlines/capacity/serializers.py
+++ ZipAirlines/capacity/serializers.py
@@ -2,12 +2,13 @@ from rest_framework import serializers
 from .mathLib import findCapacity
 
 class CapacitySerializer(serializers.Serializer):
-    planeId = serializers.ListField(
-        child = serializers.IntegerField(min_value=0, max_value=10)
-    )
-    passengerNum = serializers.ListField(
-        child = serializers.IntegerField(min_value=0)
-    )
+    planeId = serializers.IntegerField(min_value=0, max_value=10)
+    passengerNum = serializers.IntegerField(min_value=0)
     litersPerMinute = serializers.FloatField(required=False)
     minutesOfFlight = serializers.FloatField(required=False)
+
+    def create(self, validated_data):
+        litersPerMinute, minutesOfFlight = findCapacity(self.planeId, self.passengerNum)
+        print(litersPerMinute, minutesOfFlight)
+
 
--- ZipAirlines/capacity/views.py
+++ ZipAirlines/capacity/views.py
@@ -1,13 +1,23 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status #TODO translate http status from number to readible
+from rest_framework import status
 from capacity.serializers import CapacitySerializer
 
 class CapacityView(APIView):
+    def get(self, request):
+        return Response(
+            'please send query to endpoint URL:port/capacity followed' \
+            'by ordered planeId and passanger numebrs respectively',
+            status=400
+        )
+
+
     def post(self, request):
         print(request.query_params)
-        serializer = CapacitySerializer(data=request.query_params)
+        serializer = CapacitySerializer(data=request.query_params, many=True)
         serializer.is_valid(raise_exception=True)
-        print(serializer)
+        resp = serializer.save()
+        print(resp)
+        print(serializer.validated_data)
         return Response('hello')
 
